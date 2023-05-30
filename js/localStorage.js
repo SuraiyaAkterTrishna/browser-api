80c04093d12f37c5745f6a0d2d48b49b48d2c8db
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-30 15:34:48 +0600

    add, remove and clear from local storage

--- js/localStorage.js
+++ js/localStorage.js
@@ -5,8 +5,17 @@ const addToLocalStorage = (key, id) => {
     localStorage.setItem(key, inputValue);
 };
 document.getElementById('btn-add-name').addEventListener('click', function(){
-    addToLocalStorage('name', 'name-field')
+    addToLocalStorage('name', 'name-field');
 });
 document.getElementById('btn-add-age').addEventListener('click', function(){
     addToLocalStorage('age', 'age-field');
+});
+document.getElementById('btn-remove-name').addEventListener('click', function(){
+    localStorage.removeItem('name');
+});
+document.getElementById('btn-remove-age').addEventListener('click', function(){
+    localStorage.removeItem('age');
+});
+document.getElementById('btn-clear-ls').addEventListener('click', function(){
+    localStorage.clear();
 });
