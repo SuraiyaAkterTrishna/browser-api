1d9378f785a80419c9b33f2a1481eeb566e7b8d6
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-30 15:20:00 +0600

    add to local storage

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,1 +1,12 @@
-localStorage.getItem('name');
+const addToLocalStorage = (key, id) => {
+    const inputField = document.getElementById(id);
+    const inputValue = inputField.value;
+    // add local storage 
+    localStorage.setItem(key, inputValue);
+};
+document.getElementById('btn-add-name').addEventListener('click', function(){
+    addToLocalStorage('name', 'name-field')
+});
+document.getElementById('btn-add-age').addEventListener('click', function(){
+    addToLocalStorage('age', 'age-field');
+});
