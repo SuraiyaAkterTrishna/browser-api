f341e738ed8f4a4f3d227a3cf92608bedc320191
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-31 16:38:08 +0600

    practice 3 done Alhamdulillah

--- practice2.js
+++ practice2.js
@@ -7,12 +7,35 @@ const getElementValueById = (id) => {
 document.getElementById('btn-add').addEventListener('click', function(){
     const product = getElementValueById('product');
     const quantity = getElementValueById('quantity');
-    displayProduct(product, quantity);
-    localStorage.setItem(product, quantity);
+    addProductToDisplay(product, quantity);
+    // localStorage.setItem(product, quantity);
+    saveCartToLocalStorage(product, quantity);
 });
-const displayProduct = (product, quantity) => {
+const getCartFromLocalStorage = () => {
+    const savedCart = localStorage.getItem('cart');
+    let cart = {};
+    if(savedCart){
+        cart = JSON.parse(savedCart);
+    }
+    return cart;
+};
+const saveCartToLocalStorage = (product, quantity) => {
+    const cart = getCartFromLocalStorage();
+    cart[product] = quantity;
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
+};
+const addProductToDisplay = (product, quantity) => {
     const productContainer = document.getElementById('product-container');
     const li = document.createElement('li');
     li.innerText = `${product} : ${quantity}`;
     productContainer.appendChild(li);
 }
+const displayStoredProduct = () => {
+    const cart = getCartFromLocalStorage('cart');
+    for(const product in cart){
+        const quantity = cart[product];
+        addProductToDisplay(product, quantity);
+    }
+}
+displayStoredProduct();
